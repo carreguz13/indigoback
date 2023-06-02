ed9f1a356c0a4e727b3719f089fd9b3b7655a443
carreguz13 <carreguz13@gmail.com>
2023-06-01 22:20:23 -0700

    set termino de agrgegar en mysql workbech los tres tipos de grupos y sus respectivas rutas en index.js con get y post, trabajan sin errores

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express from "express"
 import mysql from "mysql"
+import cors from "cors"
 
 const app = express()
 
 
 //start connection with mysql
 //ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'your_current_password';
+
+app.use(express.json())
+app.use(cors())
 
 const db = mysql.createConnection({
     host: "localhost",
@@ -24,21 +28,70 @@ app.get("/", (req, res) => {
   });
 
 
-//we´r getting the array of indigo bellydance in json form
+
+
+//we´r getting the array of indigo bellydance, folklore & ballet in json form
 app.get("/bellydance", (req, res)=>{
-const q = "SELECT * FROM bellydance"
+const q = "SELECT * FROM indigo.bellydance"
 db.query(q, (err, data)=>{
 if(err) return res.json(err)
 return res.json(data)
 })
 })
 
+app.get("/folklore", (req, res)=>{
+    const q = "SELECT * FROM indigo.folklore"
+    db.query(q, (err, data)=>{
+    if(err) return res.json(err)
+    return res.json(data)
+    })
+    })
+    
+app.get("/ballet", (req, res)=>{
+    const q = "SELECT * FROM indigo.ballet"
+    db.query(q, (err, data)=>{
+    if(err) return res.json(err)
+    return res.json(data)
+    })
+    })
+
+
 //now we need to create the post method because we want to put information into this list in the server with post 
 app.post("/bellydance", (req, res)=>{
 const q = "INSERT INTO bellydance (nombre, edad, grupo) VAlUES (?)"
-const values = ["joshua", "18", "bellydance"]
+const values = [
+    req.body.nombre,
+    req.body.edad,
+    req.body.grupo
+]
 db.query(q, [values], (err,data) =>{
     if(err) return res.json(err)
 return res.json("se postulo bien perro")
 })
 })
+
+app.post("/folklore", (req, res)=>{
+    const q = "INSERT INTO folklore (nombre, edad, grupo) VAlUES (?)"
+    const values = [
+        req.body.nombre,
+        req.body.edad,
+        req.body.grupo
+    ]
+    db.query(q, [values], (err,data) =>{
+        if(err) return res.json(err)
+    return res.json("se postulo bien perro")
+    })
+    })
+
+    app.post("/ballet", (req, res)=>{
+        const q = "INSERT INTO ballet (nombre, edad, grupo) VAlUES (?)"
+        const values = [
+            req.body.nombre,
+            req.body.edad,
+            req.body.grupo
+        ]
+        db.query(q, [values], (err,data) =>{
+            if(err) return res.json(err)
+        return res.json("se postulo bien perro")
+        })
+        })
